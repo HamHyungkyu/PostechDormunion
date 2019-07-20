59173eed46a4713ae6a940eb1acf62f98306d563
HyungkyuHam <hhk971@postech.ac.kr>
2019-07-20 22:09:56 +0900

    update

--- server/index.js
+++ server/index.js
@@ -22,6 +22,15 @@ async function start() {
   } else {
     await nuxt.ready()
   }
+  //session 로그인 사용
+  // app.use(express.cookieParser())
+  // app.use(express.session({
+  //   key: ‘sid’, // 세션키
+  //   secret: ‘secret’, // 비밀키
+  //   cookie: {
+  //     maxAge: 1000 * 60 * 60 // 쿠키 유효기간 1시간
+  //   }
+  // }));
 
   //shops(배달업체) route middleware 연결
   app.use('/api/delivery', shops)
